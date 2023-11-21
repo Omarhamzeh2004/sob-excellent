ec55b64240b0e0c8d96c89cd4cb80a0753cf7b5a
omar <OH178@live.mdx.ac>
2023-11-21 12:27:03 +0400

    added variable and statements and fixed some errors

--- cowbulls_incomplete.py
+++ cowbulls_incomplete.py
@@ -1,13 +1,20 @@
 import random
 
 def compare_numbers(number, user_guess):
-    ## your code here
+    cow = 0 # added variable for cow
+    bull = 0 # added variable for bull
+    for i in range(4): # added for for loop
+        if user_guess[i] == number[i]: # add if statement that if user guesses one digit number placement right, bull variable will increase by one
+            bull+=1 # bull variable will increase by one if statement above is met
+        elif user_guess[i] in number: # add elif statement that if user input value is in the number value, it will increase the cow variable by one
+            cow+=1 # cow variable will increase by one if statement above is met
+        cowbull = (cow,bull) # added cowbull variable that stores cow and bull variable
     return cowbull
 
 playing = True #gotta play the game
 number = str(random.randint(0,9999)) #random 4 digit number
 guesses = 0
-print number
+print(number) #added brackets
 
 print("Let's play a game of Cowbull!") #explanation
 print("I will generate a number, and you have to guess the numbers one digit at a time.")
@@ -16,7 +23,7 @@ print("The game ends when you get 4 bulls!")
 print("Type exit at any prompt to exit.")
 
 while playing:
-    user_guess = raw_input("Give me your best guess!")
+    user_guess = input("Give me your best guess!") #removed raw_
     if user_guess == "exit":
         break
     cowbullcount = compare_numbers(number,user_guess)
