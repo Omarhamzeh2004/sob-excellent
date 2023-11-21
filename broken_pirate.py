16ed5e5fa2a5d48a399005655f7ee208c9ad989d
omar <OH178@live.mdx.ac>
2023-11-21 11:14:58 +0400

    changed brackets and changed syntax

--- broken_pirate.py
+++ broken_pirate.py
@@ -1,5 +1,5 @@
-greeting = input("Hello, possible pirate! What's the password?)
-if greeting in ["Arrr!"):
+greeting = input("Hello, possible pirate! What's the password?") #added " at the end and added bracket
+if greeting in ["Arrr!"]: #changed ) to ]
 	print("Go away, pirate.")
-elif
-print("Greetings, hater of pirates!")
+else: #changed elif to else and added :
+	print("Greetings, hater of pirates!") #added more space
