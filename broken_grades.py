c25ce4634747a7c58ec93acfe0488533dcda55d6
omar <OH178@live.mdx.ac>
2023-11-21 11:08:01 +0400

    changed syntax and logical errors

--- broken_grades.py
+++ broken_grades.py
@@ -21,26 +21,26 @@
 
 exam_one = int(input("Input exam grade one: "))
 
-exam_two = input("Input exam grade two: "))
+exam_two = int(input("Input exam grade two: ")) #remove extra brackets/ added int
 
-exam_3 = str(input("Input exam grade three: "))
+exam_3 = int(input("Input exam grade three: ")) #changed str to int
 
-grades = [exam_one exam_two exam_three]
+grades = [exam_one,exam_two,exam_3] #added comas and changed exam three to exam_3
 sum = 0
-for grade in grade:
+for grade in grades: #changed grade to grades
   sum = sum + grade
 
-avg = sum / len(grdes)
+avg = sum / len(grades) #changed grds to grades
 
 if avg >= 90:
     letter_grade = "A"
-elif avg >= 80 and avg < 90
+elif avg >= 80 and avg < 90: #added :
     letter_grade = "B"
 elif avg > 69 and avg < 80:
-    letter_grade = "C'
+    letter_grade = "C" #changed ' to "
 elif avg <= 69 and avg >= 65:
     letter_grade = "D"
-elif:
+else: #changed elif to else
     letter_grade = "F"
 
 for grade in grades:
@@ -50,7 +50,7 @@ for grade in grades:
 
     print("Grade: " + letter_grade)
 
-if letter-grade is "F":
-    print "Student is failing."
+if letter_grade == "F": #changed letter-grade to letter_grade/ changed "is" to ==
+    print("Student is failing.")#add brackets
 else:
-    print "Student is passing."
+    print("Student is passing.")#add brackets
